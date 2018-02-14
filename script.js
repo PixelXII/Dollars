59e8cd8ab5faee9ed105c5fb6de295098969960d
Levi Thorne <35429528+PixelXII@users.noreply.github.com>
2018-02-13 18:21:59 -0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+function picto() {
+ document.getElementById("pass").submit;
+ if(document.getElementById("pass").value === "baccano") {
+  console.log(document.getElementById("pass").value);
+  reDirect();
+      }
+ else {
+  return false;
+  alert("Incorrect password.");
+  
+ }
+}
