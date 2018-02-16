7912b4d8bd57130eadfcbbfc13e1f0068bfd2175
Levi Thorne <35429528+PixelXII@users.noreply.github.com>
2018-02-16 12:50:09 -0800

    Update passwordManagement.js

--- passwordManagement.js
+++ passwordManagement.js
@@ -1,14 +1,17 @@
 function reDirect() {
  location.assign("./forum.html");  
 }
+
 function submitForm() {
    var entryLogin = document.getElementById("pword").value;
     if(entryLogin == 'baccano') {
       reDirect();
     }
+ 
     else {
      alert("\"" + entryLogin + "\" is not the password.");
      return false;
     }
+ 
     console.log(entryLogin);
   }
