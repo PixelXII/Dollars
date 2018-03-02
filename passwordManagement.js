d659cc0f14d31d80b54f432903027188ac73108f
Levi Thorne <35429528+PixelXII@users.noreply.github.com>
2018-03-01 20:56:53 -0800

    Update passwordManagement.js

--- passwordManagement.js
+++ passwordManagement.js
@@ -5,7 +5,7 @@
 function submitForm() {
   var entryLogin = document.getElementById("pword").value;
     if(entryLogin == 'baccano') {
-      window.location("./forum.html");
+      window.location = "./forum.html"
     }
  var spam = entryLogin.length - 8;
  if(spam > 8) {
