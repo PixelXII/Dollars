cb0c87238b1526492249eb65a707e393f1193b6d
altD <35429528+PixelXII@users.noreply.github.com>
2018-03-28 14:12:25 -0700

    Update passwordManagement.js

--- passwordManagement.js
+++ passwordManagement.js
@@ -6,14 +6,7 @@ function submitForm() {
   var entryLogin = document.getElementById("pword").value;
     if(entryLogin == 'baccano') {
       window.location = "./forum.html"
-    }
- var spam = entryLogin.length - 8;
- if(spam > 8) {
-  alert("The password is " + spam + " characters too long.");
- }
- else {
+    } else {
   document.getElementById("incorrectPword").style.color = "red";
-   
- }
-    console.log(entryLogin);
+    }
   }
